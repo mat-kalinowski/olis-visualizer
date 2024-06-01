c0297c36d7a232654ca968b9514e8987aeb50ca8
Mateusz Kalinowski <mateusz.kalinowski@arm.com>
2024-06-01 15:25:48 +0200

    Basic request with got client

--- server.js
+++ server.js
@@ -7,61 +7,64 @@ const fastify = Fastify({
 })
 
 fastify.get('/', async function handler (request, reply) {
-  let payload = JSON.stringify({
-    "category_id": 1,
-    "subcategory_id": 1,
-    "search_in": "contractor",
-    "keyword": "SB",
-    "limit": 12,
-    "start_date_selected": "2020-05-25T11:12:45.119Z",
-    "end_date_selected": "2024-05-31T22:00:00.000Z",
-    "sort_type": "position",
-    "sort_order": "asc"
-  });
-  const {data} = await got.post({
+  // let payload = JSON.stringify({
+  //   "category_id": 1,
+  //   "subcategory_id": 1,
+  //   "search_in": "contractor",
+  //   "keyword": "SB",
+  //   "limit": 12,
+  //   "start_date_selected": "2020-05-25T11:12:45.119Z",
+  //   "end_date_selected": "2024-05-31T22:00:00.000Z",
+  //   "sort_type": "position",
+  //   "sort_order": "asc"
+  // });
+  // console.log(`data: ${data}`)
+
+  var data = {
+    category_id: 1,
+    subcategory_id: 1,
+    search_in: "contractor",
+    keyword: "SB",
+    limit: 12,
+    start_date_selected: "2020-05-25T11:12:45.119Z",
+    end_date_selected: "2024-05-31T22:00:00.000Z",
+    sort_type: "position",
+    sort_order: "asc",
+  };
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
     url: 'https://www.olis.pl/api/search',
     headers: { 
       'referer': 'https://www.olis.pl/charts/oficjalna-lista-sprzedazy/albumy', 
       'Content-Type': 'application/json'
     },
-	  json: payload,
+    data : data
+  };
+
+  // var response_data = await axios.request(config)
+  // .then((response) => {
+  //   // console.log(JSON.stringify(response.data));
+  //   console.log("after response")
+  //   return JSON.stringify(response.data)
+  // })
+  // .catch((error) => {
+  //   console.log(error);
+  // });
+
+  var response_data = await got.post({
+    url: 'https://www.olis.pl/api/search',
+    headers: { 
+      'referer': 'https://www.olis.pl/charts/oficjalna-lista-sprzedazy/albumy', 
+      'Content-Type': 'application/json'
+    },
+    json: data,
   })
 
-  console.log(`data: ${data}`)
-
-// let data = JSON.stringify({
-//   "category_id": 1,
-//   "subcategory_id": 1,
-//   "search_in": "contractor",
-//   "keyword": "SB",
-//   "limit": 12,
-//   "start_date_selected": "2020-05-25T11:12:45.119Z",
-//   "end_date_selected": "2024-05-31T22:00:00.000Z",
-//   "sort_type": "position",
-//   "sort_order": "asc"
-// });
-
-// let config = {
-//   method: 'post',
-//   maxBodyLength: Infinity,
-//   url: 'https://www.olis.pl/api/search',
-//   headers: { 
-//     'referer': 'https://www.olis.pl/charts/oficjalna-lista-sprzedazy/albumy', 
-//     'Content-Type': 'application/json'
-//   },
-//   data : data
-// };
-
-// axios.request(config)
-// .then((response) => {
-//   console.log(JSON.stringify(response.data));
-// })
-// .catch((error) => {
-//   console.log(error);
-// });
-
-
-  return { data: data }
+  console.log("before return")
+  console.log(response_data)
+  return { data: response_data.body }
 })
 
 try {
